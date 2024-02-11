8419fc24250bfe6c847b12e9fa72ce03b9d489ba
mugi <m.sano@newbees.jp>
2024-02-11 18:05:37 +0900

    feat(collector): create sample db

--- aozora/cmd/aozora-collector/main.go
+++ aozora/cmd/aozora-collector/main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"archive/zip"
-	"bytes"
-	"errors"
+	"database/sql"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
-	"path"
-	"regexp"
+	"os"
 	"strings"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/ikawaha/kagome-dict/ipa"
+	"github.com/ikawaha/kagome/v2/tokenizer"
+	_ "github.com/mattn/go-sqlite3"
 	"golang.org/x/text/encoding/japanese"
 )
 
@@ -27,119 +23,203 @@ type Entry struct {
 }
 
 func main() {
-	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
-
-	entries, err := findEntries(listURL)
+	db, err := sql.Open("sqlite3", "database.sqlite")
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, entry := range entries {
-		content, err := extractText(entry.ZipURL)
+	defer db.Close()
+
+	queries := []string{
+		`DROP TABLE authors`,
+		`DROP TABLE contents`,
+		`DROP TABLE contents_fts`,
+		`CREATE TABLE IF NOT EXISTS authors(author_id TEXT, author TEXT, PRIMARY KEY (author_id))`,
+		`CREATE TABLE IF NOT EXISTS contents(author_id TEXT, title_id TEXT, title TEXT, content TEXT, PRIMARY KEY (author_id, title_id))`,
+		`CREATE VIRTUAL TABLE IF NOT EXISTS contents_fts USING fts4(words)`,
+	}
+	for _, query := range queries {
+		_, err = db.Exec(query)
 		if err != nil {
-			log.Println(err)
-			continue
+			log.Fatal(err)
 		}
-		fmt.Println(entry.InfoURL)
-		fmt.Println(content)
 	}
+	b, err := os.ReadFile("ababababa.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err = japanese.ShiftJIS.NewDecoder().Bytes(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	content := string(b)
+	res, err := db.Exec(`INSERT INTO contents(author_id, title_id, title, content) values(?, ?, ?, ?)`,
+		"000879",
+		"14",
+		"あばばばば",
+		content,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	docID, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	t, err := tokenizer.New(ipa.Dict(), tokenizer.OmitBosEos())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	seg := t.Wakati(content)
+	_, err = db.Exec(`
+		INSERT INTO contents_fts(docid, words) values(?, ?)`,
+		docID,
+		strings.Join(seg, " "),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	query := "虫 AND ココア"
+	rows, err := db.Query(`
+		SELECT
+			c.title
+		FROM
+			contents c
+		INNER JOIN contents_fts f
+			ON c.rowid = f.docid
+			AND words MATCH ?
+	`, query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var title string
+		err := rows.Scan(&title)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(title)
+	}
+
+	// ========
+
+	// listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
+
+	// entries, err := findEntries(listURL)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// for _, entry := range entries {
+	// 	content, err := extractText(entry.ZipURL)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 		continue
+	// 	}
+	// 	fmt.Println(entry.InfoURL)
+	// 	fmt.Println(content)
+	// }
 }
 
-func extractText(zipURL string) (string, error) {
-	resp, err := http.Get(zipURL)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
+// func extractText(zipURL string) (string, error) {
+// 	resp, err := http.Get(zipURL)
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	defer resp.Body.Close()
 
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
+// 	b, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return "", err
+// 	}
 
-	r, err := zip.NewReader(bytes.NewReader(b), int64(len(b)))
-	if err != nil {
-		return "", err
-	}
+// 	r, err := zip.NewReader(bytes.NewReader(b), int64(len(b)))
+// 	if err != nil {
+// 		return "", err
+// 	}
 
-	for _, file := range r.File {
-		if path.Ext(file.Name) == ".txt" {
-			f, err := file.Open()
-			if err != nil {
-				return "", err
-			}
-			b, err := ioutil.ReadAll(f)
-			f.Close()
-			if err != nil {
-				return "", err
-			}
-			b, err = japanese.ShiftJIS.NewDecoder().Bytes(b)
-			if err != nil {
-				return "", err
-			}
-			return string(b), nil
-		}
-	}
-	return "", errors.New("contents not found")
-}
+// 	for _, file := range r.File {
+// 		if path.Ext(file.Name) == ".txt" {
+// 			f, err := file.Open()
+// 			if err != nil {
+// 				return "", err
+// 			}
+// 			b, err := ioutil.ReadAll(f)
+// 			f.Close()
+// 			if err != nil {
+// 				return "", err
+// 			}
+// 			b, err = japanese.ShiftJIS.NewDecoder().Bytes(b)
+// 			if err != nil {
+// 				return "", err
+// 			}
+// 			return string(b), nil
+// 		}
+// 	}
+// 	return "", errors.New("contents not found")
+// }
 
-func findEntries(siteURL string) ([]Entry, error) {
-	doc, err := goquery.NewDocument(siteURL)
-	if err != nil {
-		return nil, err
-	}
+// func findEntries(siteURL string) ([]Entry, error) {
+// 	doc, err := goquery.NewDocument(siteURL)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	pat := regexp.MustCompile(`.*cards/([0-9]+)/card([0-9]+).html$`)
-	entries := []Entry{}
-	doc.Find("ol li a").Each(func(i int, elem *goquery.Selection) {
-		token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
-		if len(token) != 3 {
-			return
-		}
-		title := elem.Text()
-		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html", token[1], token[2])
-		author, zipURL := findAuthorAndZipURL(pageURL) // 作者とZIPファイルのURLを得る
-		if zipURL != "" {
-			entries = append(entries, Entry{
-				AuthorID: token[1],
-				Author:   author,
-				TitleID:  token[2],
-				Title:    title,
-				InfoURL:  pageURL,
-				ZipURL:   zipURL,
-			})
-		}
-	})
-	return entries, nil
-}
+// 	pat := regexp.MustCompile(`.*cards/([0-9]+)/card([0-9]+).html$`)
+// 	entries := []Entry{}
+// 	doc.Find("ol li a").Each(func(i int, elem *goquery.Selection) {
+// 		token := pat.FindStringSubmatch(elem.AttrOr("href", ""))
+// 		if len(token) != 3 {
+// 			return
+// 		}
+// 		title := elem.Text()
+// 		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html", token[1], token[2])
+// 		author, zipURL := findAuthorAndZipURL(pageURL) // 作者とZIPファイルのURLを得る
+// 		if zipURL != "" {
+// 			entries = append(entries, Entry{
+// 				AuthorID: token[1],
+// 				Author:   author,
+// 				TitleID:  token[2],
+// 				Title:    title,
+// 				InfoURL:  pageURL,
+// 				ZipURL:   zipURL,
+// 			})
+// 		}
+// 	})
+// 	return entries, nil
+// }
 
-func findAuthorAndZipURL(siteURL string) (string, string) {
-	log.Println("query", siteURL)
-	doc, err := goquery.NewDocument(siteURL)
-	if err != nil {
-		return "", ""
-	}
+// func findAuthorAndZipURL(siteURL string) (string, string) {
+// 	log.Println("query", siteURL)
+// 	doc, err := goquery.NewDocument(siteURL)
+// 	if err != nil {
+// 		return "", ""
+// 	}
 
-	author := doc.Find("table[summary=作家データ] tr:nth-child(2) td:nth-child(2)").Text()
+// 	author := doc.Find("table[summary=作家データ] tr:nth-child(2) td:nth-child(2)").Text()
 
-	zipURL := ""
-	doc.Find("table.download a").Each(func(n int, elm *goquery.Selection) {
-		href := elm.AttrOr("href", "")
-		if strings.HasSuffix(href, ".zip") {
-			zipURL = href
-		}
-	})
+// 	zipURL := ""
+// 	doc.Find("table.download a").Each(func(n int, elm *goquery.Selection) {
+// 		href := elm.AttrOr("href", "")
+// 		if strings.HasSuffix(href, ".zip") {
+// 			zipURL = href
+// 		}
+// 	})
 
-	if zipURL == "" {
-		return author, ""
-	}
-	if strings.HasPrefix(zipURL, "http://") || strings.HasPrefix(zipURL, "https://") {
-		return author, zipURL
-	}
+// 	if zipURL == "" {
+// 		return author, ""
+// 	}
+// 	if strings.HasPrefix(zipURL, "http://") || strings.HasPrefix(zipURL, "https://") {
+// 		return author, zipURL
+// 	}
 
-	u, err := url.Parse(siteURL)
-	if err != nil {
-		return author, ""
-	}
-	u.Path = path.Join(path.Dir(u.Path), zipURL)
-	return author, u.String()
-}
+// 	u, err := url.Parse(siteURL)
+// 	if err != nil {
+// 		return author, ""
+// 	}
+// 	u.Path = path.Join(path.Dir(u.Path), zipURL)
+// 	return author, u.String()
+// }
--- aozora/go.mod
+++ aozora/go.mod
@@ -2,12 +2,12 @@ module github.com/NBsano/aozora-search
 
 go 1.21.5
 
-require (
-	github.com/PuerkitoBio/goquery v1.8.1
-	golang.org/x/text v0.14.0
-)
+require golang.org/x/text v0.14.0
+
+require github.com/ikawaha/kagome-dict v1.0.9 // indirect
 
 require (
-	github.com/andybalholm/cascadia v1.3.1 // indirect
-	golang.org/x/net v0.7.0 // indirect
+	github.com/ikawaha/kagome-dict/ipa v1.0.10
+	github.com/ikawaha/kagome/v2 v2.9.5
+	github.com/mattn/go-sqlite3 v1.14.22
 )
